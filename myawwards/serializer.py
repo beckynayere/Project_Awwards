4177fc4ab8901ac19af1733f86d87aa68f85d73a
becky nayere <rebecca.nayere@gmail.com>
2020-10-24 17:28:43 +0300

    add class userserializer

--- myawwards/serializer.py
+++ myawwards/serializer.py
@@ -7,3 +7,10 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['name', 'profile_picture', 'bio', 'location', 'contact']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer()
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'profile')
