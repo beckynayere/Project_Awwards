598323de29bebec5bd7083272a8ae3f05c14b3e5
becky nayere <rebecca.nayere@gmail.com>
2020-10-24 15:07:22 +0300

    add import django

--- myawwards/urls.py
+++ myawwards/urls.py
@@ -1,3 +1,13 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+
+
+]
--- myawwards/views.py
+++ myawwards/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http  import HttpResponse, Http404,HttpResponseRedirect,JsonResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
