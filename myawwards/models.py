3726e089d6b63348f55c8afba2587c2c2ed950b5
becky nayere <rebecca.nayere@gmail.com>
2020-10-24 17:49:11 +0300

    define userserializer

--- myawwards/models.py
+++ myawwards/models.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 from pyuploadcare.dj.models import ImageField
 import datetime as dt
 
+# Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
@@ -28,4 +29,29 @@ class Profile(models.Model):
 
 
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=155)
+    url = models.URLField(max_length=255)
+    description = models.TextField(max_length=255)
+    technologies = models.CharField(max_length=200, blank=True)
+    photo = ImageField(manual_crop='1280x720')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+
+    @classmethod
+    def all_posts(cls):
+        return cls.objects.all()
+
+    def save_post(self):
+        self.save()
--- myawwards/serializer.py
+++ myawwards/serializer.py
@@ -4,20 +4,23 @@ from django.contrib.auth.models import User
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    profile = ProfileSerializer()
+    # profile = ProfileSerializer()
     class Meta:
         model = Profile
         fields = ['name', 'profile_picture', 'bio', 'location', 'contact']
 
 
 class UserSerializer(serializers.ModelSerializer):
-    profile = ProfileSerializer()
+    profile = ProfileSerializer(read_only=True)
+    posts = PostSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'profile')
 
 
- class PostSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'title', 'url', 'description', 'technologies', 'photo', 'date', 'user']
+        fields = ['id', 'url', 'username', 'profile', 'posts']
+        # fields = ['id', 'title', 'url', 'description', 'technologies', 'photo', 'date', 'user']
