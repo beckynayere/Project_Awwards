b132e2c0d9e6b94f79e2ce20cf367099d514fbf0
becky nayere <rebecca.nayere@gmail.com>
2020-10-24 15:20:24 +0300

    create templates

--- awwards/settings.py
+++ awwards/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'myawwards',
     'bootstrap4',
+    'crispy_forms',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
--- myawwards/views.py
+++ myawwards/views.py
@@ -2,3 +2,22 @@ from django.http  import HttpResponse, Http404,HttpResponseRedirect,JsonResponse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def index(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+    else:
+        form = PostForm()
+
+    try:
+        posts = Post.objects.all()
+        posts = posts[::-1]
+        a_post = random.randint(0, len(posts)-1)
+        random_post = posts[a_post]
+        print(random_post.photo)
+    except Post.DoesNotExist:
+        posts = None
+    return render(request, 'index.html', {'posts': posts, 'form': form, 'random_post': random_post})
