21b77c3dca5a0d3c5be2b8daa0787142224e6489
becky nayere <rebecca.nayere@gmail.com>
2020-10-24 20:46:48 +0300

    create forms

--- myawwards/forms.py
+++ myawwards/forms.py
@@ -1,11 +1,3 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-admin.site.register(models.Profile)
-admin.site.register(models.Post)
-# admin.site.register(models.Rating)
-
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
