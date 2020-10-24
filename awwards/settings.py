89fe1b3bdda3f434164aac0004f997534e2c311a
becky nayere <rebecca.nayere@gmail.com>
2020-10-25 01:18:21 +0300

    configure heroku

--- awwards/settings.py
+++ awwards/settings.py
@@ -192,3 +192,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning'
 }
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
