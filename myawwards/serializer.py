f800a0ab337dcccf512b9e7e4a8bafb47bb1626c
becky nayere <rebecca.nayere@gmail.com>
2020-10-24 17:30:34 +0300

    add a class for post serializer

--- myawwards/serializer.py
+++ myawwards/serializer.py
@@ -14,3 +14,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'profile')
+
+
+ class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'url', 'description', 'technologies', 'photo', 'date', 'user']
