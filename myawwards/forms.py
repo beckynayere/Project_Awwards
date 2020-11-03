24a130fe2367fd6850d0874267a21e96b6c479ba
becky nayere <rebecca.nayere@gmail.com>
2020-11-03 11:33:57 +0300

    add project form

--- myawwards/forms.py
+++ myawwards/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post, Profile, Rating
+from .models import Post, Profile, Rating, Project
 from pyuploadcare.dj.forms import ImageField
 
 
@@ -11,6 +11,11 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['user', 'pub_date', 'profile']
 
 
 class PostForm(forms.ModelForm):
