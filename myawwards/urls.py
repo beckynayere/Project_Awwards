be4b47d93367182a953539be7473af38be558c14
becky nayere <rebecca.nayere@gmail.com>
2020-11-03 11:50:13 +0300

    add repath

--- myawwards/urls.py
+++ myawwards/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include,re_path
 from . import views
 from rest_framework import routers
 from django.conf import settings
@@ -13,16 +13,19 @@ router.register('profile', views.ProfileViewSet)
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('profile/<username>/', views.profile, name='profile'),
     path('profile/<username>/settings', views.edit_profile, name='edit'),
+    path('upload/', views.upload_project, name='upload_project'),
     path('project/<post>', views.project, name='project'),
     path('search/', views.search_project, name='search'),
     path('api/profile/', views.ProfileList.as_view()),
+    re_path('rate/(?P<project_id>\d+)', views.rate_project, name='rate'),
+    re_path('vote/(?P<project_id>\d+)', views.vote, name='vote'),
 
 
 ]
